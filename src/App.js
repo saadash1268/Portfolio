577a3783b3c1bb03b166dcdb63adf974f68469c8
Saad Ashraf <saadash1268@gmail.com>
2023-12-06 16:13:21 +0000

    intro structure complete and skills file added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Navbar from "./components/Navbar/navbar";
-import Intro from "./components/Navbar/Intro/intro";
+import Navbar from "./components/Nav/navbar";
+import Intro from "./components/Intro/intro";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Intro/>
     </div>
   );
 }
