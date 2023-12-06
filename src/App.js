a67e94baaa390e721ec2f3eff44fc4d63ef35bf8
Saad Ashraf <saadash1268@gmail.com>
2023-12-06 16:05:04 +0000

    Revert "home page structure done, need to add content and images"
    This reverts commit 8cdc99ea201e0061a17d8451007b38e45d54576c.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import Navbar from "./components/Navbar/navbar";
-import Intro from "./components/Navbar/Intro/intro";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Intro/>
     </div>
   );
 }
