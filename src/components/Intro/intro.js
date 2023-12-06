6902d6e2c89bf0432ddbf7443eed90f5ebb12274
Saad Ashraf <saadash1268@gmail.com>
2023-12-06 16:09:16 +0000

    error solve

---
+++ src/components/Intro/intro.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './intro.css';
+// import image of me for this page
+
+
+const Intro = () => {
+    return (
+        <section id="intro">
+            <div className="introContent">
+                <span className="hello">Hello, </span>
+                <span className="introText">I'm <span className="introName">Saad</span> <br/>A Full Stack Developer </span>
+                <p className="introPara"> Add a sentence introducing myself</p>
+            </div>
+           { /*for img <img src={} alt= className= */}
+        </section>
+        
+    )  
+}
+
+
+
+export default Intro;
---
+++ src/components/Nav/navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './navbar.css';
+// need to import logo once it is created 
+import { Link as ScrollLink } from 'react-scroll';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+{  /* once logo is created add here  <img src = {logo} alt="logo" className="logo" /> */}
+<div className="desktopmenu">
+    <ScrollLink to="home" smooth={true} duration={500} className="desktopMenuListItem">Home</ScrollLink>
+    <ScrollLink to="about" smooth={true} duration={500} className="desktopMenuListItem">About</ScrollLink>
+    <ScrollLink to="projects" smooth={true} duration={500} className="desktopMenuListItem">Projects</ScrollLink>
+    <ScrollLink to="blog" smooth={true} duration={500} className="desktopMenuListItem">Blog</ScrollLink>
+</div>
+<button className="desktopMenuBtn">
+    Contact Me</button>
+
+        </nav>
+    )
+}
+
+export default Navbar;
