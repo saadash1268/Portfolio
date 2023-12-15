31741498a722079edd11a70ec2e2f63b4413c718
Saad Ashraf <saadash1268@gmail.com>
2023-12-15 19:17:38 +0000

    nav bar changes

--- src/components/Nav/navbar.js
+++ src/components/Nav/navbar.js
@@ -8,9 +8,10 @@ const Navbar = () => {
         <nav className="navbar">
 {  /* once logo is created add here  <img src = {logo} alt="logo" className="logo" /> */}
 <div className="desktopmenu">
-    <ScrollLink to="home" smooth={true} duration={500} className="desktopMenuListItem">Home</ScrollLink>
     <ScrollLink to="about" smooth={true} duration={500} className="desktopMenuListItem">About</ScrollLink>
     <ScrollLink to="projects" smooth={true} duration={500} className="desktopMenuListItem">Projects</ScrollLink>
+    <ScrollLink to="contact" smooth={true} duration={500} className="desktopMenuListItem">Contact</ScrollLink>
+
     { /*<ScrollLink to="blog" smooth={true} duration={500} className="desktopMenuListItem">Blog</ScrollLink> */}
 </div>
 <button className="desktopMenuBtn">
